25d98116f7dfffac0d5b1d9ff48e4d13fc92d4ed
Kostas Rammos <rammoszzz@gmail.com>
2023-03-22 10:08:41 +0200

    Minor changes

--- Program.cs
+++ Program.cs
@@ -17,94 +17,87 @@ namespace TicketsAvailabilityAlerting
 
         static void Main(string[] args)
         {
-            try
+            ConsoleWriteIntroduction();
+
+            if (args.Length == 1 && args[0] == "soundtest")
             {
-                ConsoleWriteIntroduction();
-
-                if (args.Length == 1 && args[0] == "soundtest")
-                {
-                    Beep();
-                }
-                else if (args.Length == 1 && args[0] == "mailtest")
-                {
-                    SendMail();
-                }
-                else if (args.Length == 3 && Int32.TryParse(args[1], out timerInSec))
-                {
-                    url = args[0];
-                    arrayOfKeywords = Array.ConvertAll(args[2].Split(','), p => p.Trim());
-
-                    Timer t = new(TimerCallback, null, 0, 1000 * timerInSec);
-
-                    ConsoleWriteExit();
-                    while (Console.Read() != 'q') ;
-                }
-                else
-                {
-                    ConsoleWriteNotRightUsage();
-                }
+                Beep();
             }
-            catch (Exception ex)
+            else if (args.Length == 1 && args[0] == "mailtest")
             {
-                ConsoleWriteFailure(ex.Message);
+                SendMail();
+            }
+            else if (args.Length == 3 && Int32.TryParse(args[1], out timerInSec))
+            {
+                url = args[0];
+                arrayOfKeywords = Array.ConvertAll(args[2].Split(','), p => p.Trim());
+
+                Timer t = new(TimerCallback, null, 0, 1000 * timerInSec);
+
+                ConsoleWriteExit();
+                while (Console.Read() != 'q');
+            }
+            else
+            {
+                ConsoleWriteNotRightUsage();
             }
         }
 
 
         private static void TimerCallback(object? sender)
         {
-            ticketsSiteHtmlCode = client.DownloadString(url);
-
-            if (arrayOfKeywords.Any(ticketsSiteHtmlCode.Contains))
+            try
             {
-                ConsoleWriteSuccess();
-                Beep();
-                
-                if (!mailSent)
+                ticketsSiteHtmlCode = client.DownloadString(url);
+
+                if (arrayOfKeywords.Any(ticketsSiteHtmlCode.Contains))
                 {
-                    SendMail();
-                    mailSent = true;
+                    ConsoleWriteSuccess();
+                    Beep();
+
+                    if (!mailSent)
+                    {
+                        SendMail();
+                        mailSent = true;
+                    }
+                }
+                else
+                {
+                    ConsoleWriteFailure();
                 }
             }
-            else
+            catch (Exception ex)
             {
-                ConsoleWriteFailure("Δεν βρέθηκαν οι λέξεις-κλειδιά.");
+                ConsoleWriteException(ex);
             }
         }
 
 
         private static void SendMail()
         {
-            try
-            {
-                using MailMessage mail = new();
+            using MailMessage mail = new();
 
-                // From
-                mail.From = new MailAddress("konstantinos.rammos@haf.gr", "TicketsAvailabilityAlerting");
+            // From
+            mail.From = new MailAddress("konstantinos.rammos@haf.gr", "TicketsAvailabilityAlerting");
 
-                // To
-                mail.To.Add("k.rammos1@gmail.com");
+            // To
+            mail.To.Add("k.rammos1@gmail.com");
 
-                // Subject
-                mail.Subject = "----- Email by TicketsAvailabilityAlerting App -----";
+            // Subject
+            mail.Subject = "----- Email by TicketsAvailabilityAlerting App -----";
 
-                // Body
-                StringBuilder template = new();
-                template.AppendLine("Άνοιξαν τα εισιτήρια!");
-                mail.IsBodyHtml = false;
-                mail.Body = template.ToString();
+            // Body
+            StringBuilder template = new();
+            template.AppendLine("Άνοιξαν τα εισιτήρια!");
+            mail.IsBodyHtml = false;
+            mail.Body = template.ToString();
 
-                // SMTP settings
-                using SmtpClient smtp = new("mail.haf.gr", 587);
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential("konstantinos.rammos@haf.gr", "Qp0i1400()");
-                smtp.EnableSsl = true;
-                smtp.Send(mail);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Σφάλμα κατά την αποστολή του email. Λεπτομέρειες: {ex.Message}");
-            }
+            // SMTP settings
+            using SmtpClient smtp = new("mail.haf.gr", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("konstantinos.rammos@haf.gr", "Qp0i1400()");
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
         }
 
 
@@ -145,10 +138,10 @@ namespace TicketsAvailabilityAlerting
         }
 
 
-        private static void ConsoleWriteFailure(string error)
+        private static void ConsoleWriteFailure()
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{DateTime.Now} - {error}");
+            Console.WriteLine($"{DateTime.Now} - Δεν βρέθηκαν οι λέξεις-κλειδιά.");
             Console.ForegroundColor = ConsoleColor.White;
         }
 
@@ -166,6 +159,21 @@ namespace TicketsAvailabilityAlerting
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Πάτα \'q\' και \'enter\' για έξοδο.");
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        private static void ConsoleWriteException(Exception ex)
+        {
+            string error = "";
+            do
+            {
+                error += ex.Message + " ";
+                ex = ex.InnerException;
+            } while (ex != null);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
             Console.ForegroundColor = ConsoleColor.White;
         }
 
